582f53c634693b2857f3943fa0182e072f59534a
Pablo <pablo.ledesma.p@gmail.com>
2015-10-29 12:47:57 -0400

    add counter

--- app/assets/javascripts/script.jsx
+++ app/assets/javascripts/script.jsx
@@ -2,14 +2,19 @@ $(document).ready(function() {
   if(document.getElementById("root")!=null){
 
     var Button = React.createClass({
+      getInitialState: function() {
+        return {counter: 0};
+      },
+      handleClick: function() {
+        this.setState({ counter: this.state.counter+1});
+      },
       render: function() {
-        return (
-          <button>Go</button>
+        return(
+          <button onClick={this.handleClick}>{this.state.counter}</button>
         )
       }
     });
 
     React.render(<Button />, document.getElementById("root"));
-
   }
 });
