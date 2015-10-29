11f64bff78384f3ae93b5611d9048f04ddf101c9
Pablo <pablo.ledesma.p@gmail.com>
2015-10-29 14:07:41 -0400

    Add different buttons

--- app/assets/javascripts/script.jsx
+++ app/assets/javascripts/script.jsx
@@ -1,20 +1,40 @@
-$(document).ready(function() {
-  if(document.getElementById("root")!=null){
-
-    var Button = React.createClass({
-      getInitialState: function() {
-        return {counter: 0};
-      },
-      handleClick: function() {
-        this.setState({ counter: this.state.counter+1});
-      },
-      render: function() {
-        return(
-          <button onClick={this.handleClick}>{this.state.counter}</button>
-        )
-      }
-    });
-
-    React.render(<Button />, document.getElementById("root"));
+var Button = React.createClass({
+  localHandleClick: function() {
+    this.props.localHandleClick(this.props.increment);
+  },
+  render: function() {
+    return (
+      <button onClick={this.localHandleClick}>+{this.props.increment}</button>
+    )
   }
 });
+
+var Result = React.createClass({
+  render: function(){
+    return(
+      <div>{this.props.localCounter}</div>
+    )
+  }
+})
+
+var Main = React.createClass({
+  getInitialState: function() {
+    return {counter: 0};
+  },
+  handleClick: function(increment) {
+    this.setState({ counter: this.state.counter+increment });
+  },
+  render: function(){
+    return (
+      <div>
+        <Button localHandleClick={this.handleClick} increment={1}/>
+        <Button localHandleClick={this.handleClick} increment={5}/>
+        <Button localHandleClick={this.handleClick} increment={10}/>
+        <Button localHandleClick={this.handleClick} increment={20}/>
+        <Result localCounter={this.state.counter}/>
+      </div>
+    )
+  }
+})
+
+React.render(<Main />, document.getElementById("root"));
