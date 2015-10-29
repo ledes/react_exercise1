a313e9db74d7ffe7ee848cf9067e73db7a0c29a9
Pablo <pablo.ledesma.p@gmail.com>
2015-10-29 16:27:53 -0400

    Finish exercise 2

--- app/assets/javascripts/script.jsx
+++ app/assets/javascripts/script.jsx
@@ -65,10 +65,16 @@ var Card = React.createClass({
 });
 
 var Form = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var loginInput = React.findDOMNode(this.refs.login);
+    this.props.addCard(loginInput.value);
+    loginInput.value = '';
+  },
   render: function(){
     return (
-      <form>
-        <input placeholder="github login" />
+      <form onSubmit={this.handleSubmit}>
+        <input placeholder="github login" ref="login"/>
         <button>Add</button>
       </form>
     );
@@ -79,15 +85,17 @@ var Main2 = React.createClass({
   getInitialState: function(){
     return{logins: []};
   },
+  addCard: function(loginToAdd){
+    this.setState({logins: this.state.logins.concat(loginToAdd)});
+  },
   render: function() {
     var cards = this.state.logins.map(function(login){
       return (<Card login={login} />);
     });
     return(
       <div>
-        <Form />
+        <Form addCard={this.addCard}/>
         {cards}
-
       </div>
     )
   }
